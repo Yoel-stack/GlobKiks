8923a8eb502dbe7d0ed5dbbea06908ec0483c591
Yoel <yoelmontelongoo0@gmail.com>
2025-10-26 21:42:53 -0300

    page.tsx

--- my-app/src/app/(shop)/ofertas/page.tsx
+++ my-app/src/app/(shop)/ofertas/page.tsx
@@ -1,27 +1,41 @@
 import { Productofertas, Title, transformRawProduct } from "@/components";
 import { prisma } from "@/lib/prisma";
-
-// Forzamos SSR, no SSG
-export const revalidate = 0;
+  
 
 export default async function Ofertas() {
-  let products: ReturnType<typeof transformRawProduct>[] = [];
 
-  try {
     const currentMonth = new Date().getMonth() + 1;
 
     const productsFromDb = await prisma.product.findMany({
       where: {
-        priceOferta: { gt: 0 }, // solo productos en oferta
+        priceOferta: {
+          gt: 0, // Para que no traiga productos sin oferta o con precio 0
+        },
         mesOferta: currentMonth,
+      }, 
+      orderBy: {
+        createdAt: "desc",
       },
-      orderBy: { createdAt: "desc" },
     });
 
-    // Filtrar productos cuyo precio en oferta sea menor que el precio normal
+    // Filtrar en memoria aquellos cuyo priceOferta sea menor que price
     const filteredProducts = productsFromDb.filter(
       (p) => p.priceOferta! < p.price
     );
+
+    const products = filteredProducts.map(transformRawProduct);
+
+    return (
+      <>
+        <Title
+          title="Ofertas del mes"
+          subtitle="Todos los championes en oferta!"
+          className="mb-3"
+        />
+        <Productofertas products={products} />
+      </>
+    );
+};
 
     products = filteredProducts.map(transformRawProduct);
   } catch (error) {
