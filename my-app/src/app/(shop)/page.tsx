af98924afc0de1cc4bcf77faffe0d9d09b8e9e45
Yoel <yoelmontelongoo0@gmail.com>
2025-10-26 21:42:17 -0300

    page.tsx

--- my-app/src/app/(shop)/page.tsx
+++ my-app/src/app/(shop)/page.tsx
@@ -1,25 +1,21 @@
 import { ProductGrid, Title } from "@/components";
 import { prisma } from "@/lib/prisma";
-import { transformRawProduct } from "@/components/helpers/transformProduct";
+import { transformRawProduct } from "@/components/helpers/transformProduct"; // Creamos un helper para que me quede mas ordenado el productGrid
 
-// Forzamos SSR, no SSG
-export const revalidate = 0;
 
 export default async function Home() {
-  let products = [];
+  
+  const productsFromDb = await prisma.product.findMany();
 
-  try {
-    const productsFromDb = await prisma.product.findMany();
-    products = productsFromDb.map(transformRawProduct);
-  } catch (error) {
-    console.error("Error fetching products for Home:", error);
-  }
+  const products = productsFromDb.map(transformRawProduct);
 
   return (
     <>
       <Title title="Tienda" subtitle="Todos los championes" className="mb-3" />
       <ProductGrid products={products} />
-    </>
+    </> 
   );
-}
+};
+
+
 
